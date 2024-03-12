f6cf3ea3fb4521272c9fa599995ba5d921309cb4
dferreras439 <36524216+dferreras439@users.noreply.github.com>
2024-03-12 22:52:58 +0100

    noob loop instead of a proper cronjob honoring ephemeral containerisms

--- colabrunner.py
+++ colabrunner.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 from selenium.webdriver.common.keys import Keys
-import os
+import os, time
 
 url = 'https://colab.research.google.com/drive/1Ga2z2yIvOgJYy5auON27yfNaNqGny_Pg'
 
@@ -9,25 +9,28 @@ PASSWORD = os.getenv('PASSWORD')
 
 if __name__ == "__main__":
     
-    with SB(uc=True) as sb:
-        
-        sb.open(url)
+    while 1:
+
+        time.sleep(60*60*3)
+
+        with SB(uc=True) as sb:
+            
+            sb.open(url)
 
 
-        sb.type('input[type="email"]', USER+'\n')
-        sb.save_screenshot('0000.png')
+            sb.type('input[type="email"]', USER+'\n')
+            sb.save_screenshot('0000.png')
 
-        sb.sleep(5)
-        sb.save_screenshot('0001.png')
+            sb.sleep(5)
+            sb.save_screenshot('0001.png')
 
 
-        sb.type('input[type="password"]', PASSWORD+'\n')
-        sb.sleep(5)
-        sb.save_screenshot('0003.png')
-        
-        sb.sleep(60)
-        sb.type('body', Keys.CONTROL + Keys.F9)
-        sb.save_screenshot('0003.png')
-        
-
-        sb.sleep(15*60)
+            sb.type('input[type="password"]', PASSWORD+'\n')
+            sb.sleep(5)
+            sb.save_screenshot('0003.png')
+            
+            sb.sleep(60)
+            sb.type('body', Keys.CONTROL + Keys.F9)
+            sb.save_screenshot('0003.png')
+            
+            sb.sleep(15*60)
