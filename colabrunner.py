351a6292da3e193fefdc74f7e854cae585d9e97d
dferreras439 <36524216+dferreras439@users.noreply.github.com>
2024-03-18 12:44:29 +0100

    begin replit ver

--- colabrunner.py
+++ colabrunner.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     
     while 1:
 
-        time.sleep(60*60*3)
-
-        with SB(uc=True) as sb:
+        time.sleep(60*60*3) # sleep 3h
+        # cookies test - logging in.
+        with SB(uc=True) as sb: 
             
             sb.open(url)
 
@@ -30,7 +30,19 @@ if __name__ == "__main__":
             sb.save_screenshot('0003.png')
             
             sb.sleep(60)
-            sb.type('body', Keys.CONTROL + Keys.F9)
-            sb.save_screenshot('0003.png')
+            sb.type('body', Keys.CONTROL + Keys.ENTER)
+            sb.save_screenshot('0004.png')
             
-            sb.sleep(15*60)
+            sb.save_cookies('cookies')
+
+            sb.sleep()
+        
+        with SB(uc=True) as sb: 
+
+            sb.open(url)
+            sb.load_cookies('cookies')
+            sb.refresh()
+            sb.sleep(5)
+
+            sb.type('body', Keys.CONTROL + Keys.ENTER)
+            sb.save_screenshot('0005.png')
