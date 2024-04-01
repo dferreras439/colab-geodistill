8049a1eb35bf7d5b3618fc889fab7ab555fbe6bf
dferreras439 <36524216+dferreras439@users.noreply.github.com>
2024-04-01 15:59:25 +0200

    debug, delete screenshot

--- colabrunner.py
+++ colabrunner.py
@@ -12,24 +12,18 @@ if __name__ == "__main__":
         
         sb.open('https://replit.com/login') # Login block
         sb.click("//button[contains(., 'Continue with Google')]")
-        sb.save_screenshot('0000.png')
 
         sb.type('input[type="email"]', USER+'\n')
-        sb.save_screenshot('0001.png')
 
         sb.sleep(5)
-        sb.save_screenshot('0002.png')
 
         sb.type('input[type="password"]', PASSWORD+'\n')
-        sb.sleep(5)
-        sb.save_screenshot('0003.png') # End login block
+        sb.sleep(5) # End login block
             
         while 1:
         # Looping logic
             sb.open(URL) # Url to replit
-            sb.save_screenshot('0004.png') # Test how sb handles url
 
             sb.sleep(5)
             sb.find_element("//button[.//span[contains(text(), 'Stop')]]")
             sb.type('body', Keys.CONTROL + Keys.ENTER)
-            sb.save_screenshot('0005.png')
